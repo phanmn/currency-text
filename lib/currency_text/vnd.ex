ea2687529b18d13edb91e4e0ff29f1253e11943f
giangnt41 <giang.nt41@gmail.com>
2022-08-16 15:15:34 +0700

    feat: doc test for vnd

--- lib/currency_text/vnd.ex
+++ lib/currency_text/vnd.ex
@@ -1,4 +1,38 @@
 defmodule CurrencyText.Vnd do
+
+  @doc """
+    iex> 900000 |> CurrencyText.Vnd.to_string()
+    "Chín trăm nghìn đồng"
+
+    iex> 91024012 |> CurrencyText.Vnd.to_string()
+    "Chín mươi mốt triệu hai mươi bốn nghìn mười hai đồng"
+
+    iex> 110000 |> CurrencyText.Vnd.to_string()
+    "Một trăm mười nghìn đồng"
+
+    iex> 0 |> CurrencyText.Vnd.to_string()
+    "Không đồng"
+
+    iex> 1 |> CurrencyText.Vnd.to_string()
+    "Một đồng"
+
+    iex> 129481294121 |> CurrencyText.Vnd.to_string()
+    "Một trăm hai mươi chín tỷ bốn trăm tám mươi mốt triệu hai trăm chín mươi bốn nghìn một trăm hai mươi mốt đồng"
+
+    iex> 35 |> CurrencyText.Vnd.to_string()
+    "Ba mươi lăm đồng"
+
+    iex> 105 |> CurrencyText.Vnd.to_string()
+    "Một trăm lẻ năm đồng"
+
+    iex> 105400000 |> CurrencyText.Vnd.to_string()
+    "Một trăm lẻ năm triệu bốn trăm nghìn đồng"
+  """
+
+  def to_string(0) do
+    "Không đồng"
+  end
+
   def to_string(amount) do
     billions = amount |> div(1_000_000_000)
     millions = amount |> div(1_000_000) |> rem(1000)
