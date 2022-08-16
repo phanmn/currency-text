f5ca5c48f6559aa716b6ee88f6d4a79da0d5c75a
giangnt41 <giang.nt41@gmail.com>
2022-08-16 11:32:37 +0700

    feat: build struct for block and digit

--- lib/currency_text/vnd/block.ex
+++ lib/currency_text/vnd/block.ex
@@ -1,3 +1,8 @@
 defmodule CurrencyText.Vnd.Block do
+  use TypedStruct
 
+  typedstruct do
+    field :type, String.t(), enforce: true
+    field :value, integer(), enforce: true
+  end
 end
--- lib/currency_text/vnd/digit.ex
+++ lib/currency_text/vnd/digit.ex
@@ -1,3 +1,9 @@
 defmodule CurrencyText.Vnd.Digit do
+  use TypedStruct
 
+  typedstruct do
+    field :value, integer(), enforce: true
+    field :location, integer(), enforce: true
+    field :block_length, integer(), enforce: true
+  end
 end
